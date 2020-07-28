098a4ea2fbf9dc180f808b0180f5878b3d4427d4
Fadhil Muhammad <bringitdown0@gmail.com>
2020-07-28 09:07:27 +0700

    add isomorphic fetch, gonna refactor later

--- src/services/graphql.js
+++ src/services/graphql.js
@@ -1,6 +1,8 @@
 import ApolloClient from 'apollo-boost';
+import fetch from 'isomorphic-fetch';
 
 const client = new ApolloClient({
+  fetch,
   uri: 'https://tranquil-atoll-83602.herokuapp.com/query',
   request: (opr) => {
     const token = localStorage.getItem('token');
